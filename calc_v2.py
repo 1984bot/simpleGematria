b109b9482eb3d4ec81cb79cae602e6c35b027125
tm3k <60632198+tm3k@users.noreply.github.com>
2020-02-07 13:57:55 -0600

    Update calc_v2.py

--- calc_v2.py
+++ calc_v2.py
@@ -1,3 +1,6 @@
+#ERROR this code only works with 1 word and not multiple. 
+# the space between words is counted as a negative number reducing the gematria count making it wrong
+
 from gematria_dictv2 import *
 
 user_input = input('Give me a word or phrase to get the ordinal gematria in english: ')
