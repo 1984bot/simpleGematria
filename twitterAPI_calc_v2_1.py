b99140f8949dfa7bc755c7b0b24f5a0c2aad73d2
tm3k <60632198+tm3k@users.noreply.github.com>
2020-02-08 09:52:35 -0600

    Update twitterAPI_calc_v2_1.py

--- twitterAPI_calc_v2_1.py
+++ twitterAPI_calc_v2_1.py
@@ -1,43 +1,9 @@
 import tweepy
 import creds #API LOGIN CREDENTIALS
 from gematria_dictv2 import * # DICTIONARIES FOR DIFFERENT TYPES OF GEMATRIA
+from gematria_methods_module import *
 import time # used for waiting 5 seconds between tweets
 
-# Medthod to check if numbers are in the phrase
-def hasNumbers(phrase):
-    return any(char.isdigit() for char in phrase)
-
-# Method for determining ordinal gematria
-def getOrdinalGematria(phrase):
-    total = 0
-    
-    # First check to see if the phrase contains any numbers
-    if hasNumbers(phrase):
-        pass
-    else:
-
-        stripped_phrase = phrase.replace(" ", "") #replaces all spaces with nothing
-        for c in stripped_phrase:
-            ordinal = ord(c) - 96
-            total += ordinal    
-        reply_tweet = "Ordinal gematria: {}".format(total)
-        return reply_tweet
-
-# Method for determining full reduction gematria
-#def getFullReductionGematria(phrase):
-
-
-# Method for determining who it was that is requesting a tweet and replying to it with summed_word
-def replyToUsername(tweet_id,summed_word):
-    
-    #This code prints basic info to console so i can follow what the program is doing mentally and to double check that each value is assigned accordingly
-    print(f'Tweet Id: {tweet_id}')
-    print(f'Phrase: {phrase}')
-    print(f'{summed_word}')
-    print(f'Screen name: {tweet.user.name}')
-    print(f'Username: {user_id}')
-
-    api.update_status(summed_word, in_reply_to_status_id=tweet_id, auto_populate_reply_metadata=True)
 
 # Establishes connection to server and creates the API object
 auth = tweepy.OAuthHandler(creds.consumer_key, creds.consumer_secret)
