5a6f56a233082a2ec98e127ec5a954e6fcc61e66
R.1984 <1984bot+git@protonmail.com>
2020-02-10 11:56:58 -0600

    Use regular expression to remove non-alphabetic characters and whitespace

--- twitterAPI_calc_v3.py
+++ twitterAPI_calc_v3.py
@@ -2,6 +2,7 @@ import tweepy
 import creds #API LOGIN CREDENTIALS
 from gematria_dictv2 import * # DICTIONARIES FOR DIFFERENT TYPES OF GEMATRIA
 import time # used for waiting 5 seconds between tweets
+import re # used to strip out non-alphabetic characters
 
 # Medthod to check if numbers are in the phrase
 def hasNumbers(phrase):
@@ -16,14 +17,13 @@ def getGematria(phrase):
         pass
     else:
 
-        stripped_phrase = phrase.replace(" ", "") #replaces all spaces with nothing
-        for c in stripped_phrase:
+        for c in phrase:
             ordinal = ord(c) - 96
             total += ordinal    
         
         
         total2 = []
-        for n in stripped_phrase:
+        for n in phrase:
             
             if n == 'a':
                 total2.append(1)
@@ -140,8 +140,8 @@ while True:
             if keyword in line:
                 
                 temp_var = line.replace('@__tm3k #tm3k', '') # Removes the hashtag and @_tm3k from the phrase and replaces with an empty space so I can sum it up with gematria
-                phrase = temp_var.strip()
-                phrase.lower() # Strips extra spaces and makes lowercase
+                phrase = re.sub('[^a-zA-Z]', '', temp_var) # Strip all non-alphabetic characters
+                phrase = phrase.lower() # Make lowercase
                 summed_word = getGematria(phrase) # Calls gematria sum method and passes the scraped tweet string to be analyzed and stores the result in the variable summed_word
                 replyToUsername(tweet_id, summed_word) # Calls reply to username method and passes the id of the tweet containing #tm3k and the value of the letters
                 print("Sending reply...\n\n")
